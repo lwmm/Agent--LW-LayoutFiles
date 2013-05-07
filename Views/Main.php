defedf81ea46f6a3cc437b56f0f4c748fae58449
lw-michael <lw-michael@lw-michael-thinkpad>
2013-05-07 14:50:17 +0200

    icons changed from bootsrap icons to c38 icons

--- Views/Main.php
+++ Views/Main.php
@@ -20,6 +20,12 @@ class Main
 
         $view->bootstrapCSS = $this->config["url"]["media"] . "bootstrap/css/bootstrap.min.css";
         $view->bootstrapJS = $this->config["url"]["media"] . "bootstrap/js/bootstrap.min.js";
+        $view->iconUrlFolder = $this->config["url"]["media"]."pics/fatcow_icons/16x16_0440/folder.png";
+        $view->iconUrlFolderClose = $this->config["url"]["media"]."pics/fatcow_icons/16x16_0440/folder_go.png";
+        $view->iconUrlEdit = $this->config["url"]["media"]."pics/fatcow_icons/16x16_0700/pencil.png";
+        $view->iconUrlImage = $this->config["url"]["media"]."pics/fatcow_icons/16x16_0480/image.png";
+        $view->iconUrlFile = $this->config["url"]["media"]."pics/fatcow_icons/16x16_0640/page_white.png";
+        $view->iconUrlRemove = $this->config["url"]["media"]."pics/fatcow_icons/16x16_0300/cross.png";
         
         $view->dir = $dir;
         $view->dirContent = $dirContent;
